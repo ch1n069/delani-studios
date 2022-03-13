c715a5cb74b7e3b99a7994b3ab9bbb0b20cac71e
Moringa School <brunolalpachino@gmail.com>
2022-03-13 14:34:07 +0300

    added the animation to the dissapear feature

--- main.js
+++ main.js
@@ -90,7 +90,32 @@ $(document).ready(function() {
         function() {
             $("#text-overlay").removeAttr("style")
             $("#design1,h3").show()
-        })
+        }
+
+    )
+    $("#design-2").hover(function() {
+            $("#two,#2").hide();
+            $("#text-overlay1").css("opacity", 1)
+        },
+        function() {
+            $("#two,#2").show();
+            $("#text-overlay1").removeAttr("style")
+        }
+    )
+
+
+    $("#design-3").hover(function() {
+            $("#three,#3").hide();
+            $("#text-overlay2").css("opacity", 1);
+
+        },
+        function() {
+            $("#three,#3").show();
+            $("#text-overlay2").removeAttr("style")
+        }
+    )
+
+
 
 
 })
