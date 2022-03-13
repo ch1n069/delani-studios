68264053319df2de1a7abd8235c17b5fa3c89c76
Moringa School <brunolalpachino@gmail.com>
2022-03-13 12:42:45 +0300

    added js for hover animations

--- main.js
+++ main.js
@@ -30,4 +30,56 @@ $(document).ready(function() {
 
 
     )
+    $("#overlay5").hover(function() {
+            // this animates the hover to fade to
+            $(this).css("opacity", 1)
+        },
+        function() {
+            $(this).removeAttr("style")
+        }
+
+
+    )
+
+    $("#overlay6").hover(function() {
+            // this animates the hover to fade to
+            $(this).css("opacity", 1)
+        },
+        function() {
+            $(this).removeAttr("style")
+        }
+
+
+    )
+    $("#overlay7").hover(function() {
+            // this animates the hover to fade to
+            $(this).css("opacity", 1)
+        },
+        function() {
+            $(this).removeAttr("style")
+        }
+
+
+    )
+    $("#overlay8").hover(function() {
+            // this animates the hover to fade to
+            $(this).css("opacity", 1)
+        },
+        function() {
+            $(this).removeAttr("style")
+        }
+
+
+    )
+
+    $("#overlay9").hover(function() {
+            // this animates the hover to fade to
+            $(this).css("opacity", 1)
+        },
+        function() {
+            $(this).removeAttr("style")
+        }
+
+
+    )
 })
