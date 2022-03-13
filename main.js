7a574e7bedbd5715aae0355b6da56380cc13f75f
Moringa School <brunolalpachino@gmail.com>
2022-03-13 12:28:52 +0300

    ft == used jquerry to animate hover on image 2

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
 $(document).ready(function() {
-    $("#overlay").hover(function() {
+    $("#overlay2").hover(function() {
+            // this animates the hover to fade to
+            $(this).css("opacity", 1)
+        },
+        function() {
+            $(this).removeAttr("style")
+        }
+
+
+    )
+    $("#overlay3").hover(function() {
+            // this animates the hover to fade to
+            $(this).css("opacity", 1)
+        },
+        function() {
+            $(this).removeAttr("style")
+        }
+
+
+    )
+
+    $("#overlay4").hover(function() {
             // this animates the hover to fade to
             $(this).css("opacity", 1)
         },
